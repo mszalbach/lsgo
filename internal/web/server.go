21b5fe559728283aec91ee8deab25b7f3c4ee2b6
Marcel Szalbach <4-bar-killa@web.de>
2026-09-16 18:44:24 +0200

    Added Strict-Transport-Security header after nikto scan

--- internal/web/server.go
+++ internal/web/server.go
@@ -128,6 +128,7 @@ func owaspMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Cross-Origin-Resource-Policy", "same-site")
 		w.Header().Set("Permissions-Policy", "geolocation=(), camera=(), microphone=()")
 		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		w.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
 		w.Header().
 			Set("Content-Security-Policy", "default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self'; connect-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;")
 		next.ServeHTTP(w, r)
